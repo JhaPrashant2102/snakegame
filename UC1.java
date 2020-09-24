ad936ccc63951359808f16576db47be5b427a0bc
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-24 19:41:28 +0530

    first change UC1

---
+++ UC1.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import java.util.*;
+public class UC1{
+	public static void main(String args[]){
+		int rand_num = ((int)Math.floor(Math.random()*10)%6)+1;
+		System.out.println(rand_num);
+	}
+}
